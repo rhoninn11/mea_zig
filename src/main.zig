29df828cd14ee663bbb52659baff4a3885543b1d
rhoninn11 <rhoninn11@gmail.com>
2024-11-04 18:44:53 +0100

    - simple animation added

--- src/main.zig
+++ src/main.zig
@@ -21,11 +21,11 @@ const TimeTool = struct {
         return elapsed_ms;
     }
 
-    fn tickMs(self: *TimeTool) !f64 {
+    fn tickMs(self: *TimeTool) !f32 {
         try self.messsureTo();
         const time_delta_us = self.elapsedInfo();
         try self.messureFrom();
-        return time_delta_us;
+        return @floatCast(time_delta_us);
     }
 };
 
@@ -35,6 +35,65 @@ fn FreshTimeTool() !TimeTool {
     try tt.messureFrom();
     return tt;
 }
+
+const THEME = [_]rl.Color{ rl.Color.black, rl.Color.beige };
+
+const Vec2i = struct {
+    x: i32,
+    y: i32,
+
+    fn add(self: Vec2i, other: Vec2i) Vec2i {
+        return Vec2i{
+            .x = self.x + other.x,
+            .y = self.y + other.y,
+        };
+    }
+};
+
+const Circle = struct {
+    pos: Vec2i,
+    color: rl.Color,
+    height: i32,
+
+    fn basicCircle() Circle {
+        return Circle{
+            .pos = .{ .x = 100, .y = 100 },
+            .color = rl.Color.maroon,
+            .height = 100,
+        };
+    }
+
+    fn draw(self: Circle, osc: Osc) void {
+        const osc_pos = Vec2i{
+            .x = @intFromFloat(std.math.cos(osc.phase) * osc.amp),
+            .y = @intFromFloat(std.math.sin(osc.phase) * osc.amp),
+        };
+        const circle_pos = self.pos.add(osc_pos);
+        const shadow_pos = Vec2i{
+            .x = circle_pos.x,
+            .y = self.pos.y + self.height,
+        };
+        rl.drawCircle(circle_pos.x, circle_pos.y, 20, self.color);
+        rl.drawEllipse(shadow_pos.x, shadow_pos.y, 30, 5, THEME[1]);
+    }
+};
+
+const Osc = struct {
+    amp: f32,
+    phase: f32,
+
+    fn basicOsc() Osc {
+        return Osc{
+            .amp = 20,
+            .phase = 0,
+        };
+    }
+
+    fn update(self: *Osc, time_delta_ms: f32) void {
+        const delta_s = time_delta_ms / std.time.ms_per_s;
+        self.phase += delta_s * std.math.pi * 2;
+    }
+};
 
 fn raylib_loop() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -47,32 +106,33 @@ fn raylib_loop() !void {
     rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
     defer rl.closeWindow();
 
-    rl.setTargetFPS(60);
-    // !rl.windowShouldClose() - but it broke in wsl so i just use some time until close
+    // rl.setTargetFPS(60);
+
+    var red_circle = Circle.basicCircle();
+    red_circle.pos = .{ .x = 100, .y = 200 };
+
+    var main_osc = Osc.basicOsc();
+    main_osc.amp = 25;
 
     var life_time_ms: f64 = 0;
-    while (life_time_ms / std.time.ms_per_s < 10) {
+    while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         defer rl.endDrawing();
 
-        rl.clearBackground(rl.Color.white);
+        rl.clearBackground(THEME[0]);
 
         const time_delta_ms = try tt.tickMs();
 
-        life_time_ms += time_delta_ms;
+        main_osc.update(time_delta_ms);
+        life_time_ms += @floatCast(time_delta_ms);
 
         const info_template = "Congrats! You created your first window! Frame time {d:.3} ms\n";
         const info = try std.fmt.allocPrintZ(alctr, info_template, .{time_delta_ms});
         defer alctr.free(info);
 
         // std.debug.print(info_template, .{time_delta_ms});
-        rl.drawText(info, 50, 50, 20, rl.Color.light_gray);
-
-        const s_time: f64 = life_time_ms / std.time.ms_per_s;
-        const amp = 50;
-        const y_offset = @as(i32, @intFromFloat(std.math.sin(s_time) * amp));
-
-        rl.drawCircle(200, 200 + y_offset, 20, rl.Color.maroon);
+        rl.drawText(info, 50, 50, 20, THEME[1]);
+        red_circle.draw(main_osc);
     }
 }
 
