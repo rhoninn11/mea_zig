05ef7c21db9c7caefb090da8e10855e0c478f1a6
leszek <rhoninn11@gmail.com>
2024-11-25 14:45:40 +0100

    - transfer assets to fs

--- src/explore/filesystem.zig
+++ src/explore/filesystem.zig
@@ -1,50 +1,62 @@
 const std = @import("std");
-const Flags = std.fs.File.CreateFlags;
+const CopyFileOptions = std.fs.Dir.CopyFileOptions;
+const CreateFlags = std.fs.File.CreateFlags;
+const OpenFlags = std.fs.File.OpenFlags;
 
-test "how to spliting works" {
-    const name_a: []const u8 = "elo.txt";
-    const name_b: []const u8 = "fs/dir/elo.txt";
-
-    var iter_a = std.mem.split(u8, name_a, "/");
-
-    while (iter_a.next()) |elem| {
-        try std.testing.expectEqual(name_a, elem);
-    }
-
-    var iter_b = std.mem.split(u8, name_b, "/");
-    const eql = std.mem.eql;
-    const expect = std.testing.expect;
-
-    const names: [3][]const u8 = .{ "fs", "dir", "elo.txt" };
-
-    for (names) |name| {
-        const chunk = iter_b.next() orelse "";
-        try expect(eql(u8, name, chunk));
-    }
-}
+const Allocator = std.mem.Allocator;
 
 fn ensurePath(path: []const u8) !void {
     const dirname_for = std.fs.path.dirname(path) orelse ".";
-    std.debug.print("+++ dirname part is {s}\n", .{dirname_for});
     try std.fs.cwd().makePath(dirname_for);
 }
 
-test "path ensure test" {
-    // tylko nie wiem jak sprawdzać czy plik istniej xD
+fn projFile(alloc: Allocator, path: []const u8) ![]u8 {
+    const src_path = try std.fmt.allocPrint(alloc, "assets/{s}", .{path});
+    defer alloc.free(src_path);
+    const dst_path = try std.fmt.allocPrint(alloc, "fs/{s}", .{path});
+
+    try ensurePath(dst_path);
+    try std.fs.cwd().copyFile(src_path, std.fs.cwd(), dst_path, CopyFileOptions{});
+
+    return dst_path;
+}
+
+test "proj_assets_check" {
+    const src_path = "assets/test.txt";
+    const root_dir = std.fs.cwd();
+    const content_file = try root_dir.createFile(src_path, CreateFlags{ .read = true });
+    const test_data: []const u8 = "test contetnt is here";
+    _ = try content_file.write(test_data);
+
+    const dst_path = "fs/test.txt";
+    const alloc = std.testing.allocator;
+
+    const file_in_fs = try projFile(alloc, "test.txt");
+    defer alloc.free(file_in_fs);
+
+    try std.testing.expect(std.mem.eql(u8, dst_path, file_in_fs));
+    try root_dir.access(file_in_fs, OpenFlags{});
+    const check_file = try root_dir.openFile(file_in_fs, OpenFlags{ .mode = .read_only });
+
+    const b_slice = try alloc.alloc(u8, 1024);
+    defer alloc.free(b_slice);
+
+    const len = try check_file.read(b_slice);
+    const in_file_data = b_slice[0..len];
+    try std.testing.expectEqualSlices(u8, test_data, in_file_data);
+
+    try root_dir.deleteFile(src_path);
+    try root_dir.deleteFile(dst_path);
 }
 
 const SimpleData = struct {
     name: []const u8,
 };
 
-pub fn fs_explorer() !void {
-    try jsonCreateAndSave();
-}
-
 fn jsonCreateAndSave() !void {
     const first_json = "fs/deeper/file.json";
     try ensurePath(first_json);
-    const file = try std.fs.cwd().createFile(first_json, Flags{ .read = true });
+    const file = try std.fs.cwd().createFile(first_json, CreateFlags{ .read = true });
     defer file.close();
 
     std.debug.print("+++ file {s} create\n", .{first_json});
@@ -59,4 +71,8 @@ fn jsonCreateAndSave() !void {
 
     const res = try file.write(jstr_memory);
     std.debug.print("+++ what information is that {}\n", .{res});
+}
+
+pub fn fs_explorer() !void {
+    try jsonCreateAndSave();
 }
